5283886a2c638c07889ab4944894d3f4500694db
aslam21 <aslam.kocak@gmail.com>
2020-03-17 01:24:41 +0700

    nambah ul sama li

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est voluptate dolores atque impedit voluptatum. Officia voluptas accusantium, obcaecati necessitatibus nostrum, quod magni, voluptate deleniti reprehenderit enim dolorem accusamus aspernatur tempore!</p>
+    <ul>
+        <li>Ini list</li>
+    </ul>
 </body>
 </html>
